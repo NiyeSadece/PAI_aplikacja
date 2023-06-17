411190e7d63571610d1cdb539302a665fdb4cdef
Dalia Grodzka <39556362+NiyeSadece@users.noreply.github.com>
2023-06-17 20:23:24 +0200

    delete restaurant fix

--- adminlte/scripts/delete_restaurant.php
+++ adminlte/scripts/delete_restaurant.php
@@ -7,10 +7,61 @@ if ($error != 0){
 }
 
 require_once "connect.php";
-$sql = "DELETE FROM restaurants WHERE `restaurants`.`restaurant_id` = $_GET[resetaurantIdDelete]";
-$conn->query($sql);
-if ($conn->affected_rows == 0){
+$restaurantIdDelete = $_GET["restaurantIdDelete"];
+
+// Sprawdź, czy parametr został przekazany
+if (empty($restaurantIdDelete)) {
     header("location: ../pages/views/restaurants.php?restaurantDel=0");
-}else{
-    header("location: ../pages/views/restaurants.php?restaurantDel=$_GET[restaurantIdDelete]");
+    exit();
+}
+
+// Usuń stoliki
+$sql = "DELETE FROM tables WHERE restaurant_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $restaurantIdDelete);
+$stmt->execute();
+
+// Sprawdź, czy usunięto stoliki
+if ($stmt->affected_rows === -1) {
+    header("location: ../pages/views/restaurants.php?restaurantDel=0");
+    exit();
+}
+
+// Pobierz ID adresu
+$stmt = $conn->prepare("SELECT address_id FROM restaurants WHERE restaurant_id = ?");
+$stmt->bind_param("i", $restaurantIdDelete);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows === 0) {
+    header("location: ../pages/views/restaurants.php?restaurantDel=0");
+    exit();
+}
+
+$row = $result->fetch_assoc();
+$addressID = $row["address_id"];
+
+// Usuń restaurację
+$sql = "DELETE FROM restaurants WHERE restaurant_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $restaurantIdDelete);
+$stmt->execute();
+
+// Sprawdź, czy usunięto restaurację
+if ($stmt->affected_rows === 0) {
+    header("location: ../pages/views/restaurants.php?restaurantDel=0");
+    exit();
+}
+
+// Usuń adres
+$sql = "DELETE FROM address WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $addressID);
+$stmt->execute();
+
+// Sprawdź, czy usunięto adres
+if ($stmt->affected_rows === 0) {
+    header("location: ../pages/views/restaurants.php?restaurantDel=0");
+} else {
+    header("location: ../pages/views/restaurants.php?restaurantDel=$restaurantIdDelete");
 }
